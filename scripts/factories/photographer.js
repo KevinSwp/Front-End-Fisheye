6f0d80f4f4e7b71be15d709e394a5256258fd2d1
KevinSWP <thranva@gmail.com>
2022-11-22 23:26:34 +0100

    homepage text added

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,5 +1,5 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, city, country, tagline, price, portrait } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
@@ -7,11 +7,26 @@ function photographerFactory(data) {
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
+
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+
+        const town = document.createElement( 'h3' );
+        town.textContent = city + ', ' + country;
+
+        const tag = document.createElement( 'p' );
+        tag.textContent = tagline;
+
+        const prices = document.createElement( 'span' );
+        prices.textContent = price+'€'+' /jour';
+
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(town);
+        article.appendChild(tag);
+        article.appendChild(prices);
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    
+    return { name, city, country, tagline, price, picture, getUserCardDOM }
 }
