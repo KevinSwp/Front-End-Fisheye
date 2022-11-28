7cd2080de04b043dcb12728f984af8642559aaf1
KevinSWP <thranva@gmail.com>
2022-11-28 14:36:33 +0100

    json ok

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,4 +1,4 @@
-function photographerFactory(data) {
+/*function photographerFactory(data) {
     const { name, city, country, tagline, price, portrait } = data;
 
     const picture = `assets/photographers/${portrait}`;
@@ -25,6 +25,56 @@ function photographerFactory(data) {
         article.appendChild(location);
         article.appendChild(tag);
         article.appendChild(prices);
+        return (article);
+    }
+    
+    return { name, city, country, tagline, price, picture, getUserCardDOM }
+}*/
+function photographerFactory(data) {
+    const { id, name, city, country, tagline, price, portrait } = data;
+
+    const picture = `assets/photographers/${portrait}`;
+
+    function getUserCardDOM() {
+        const article = document.createElement( 'article' );
+
+        const img = document.createElement( 'img' );
+
+        //function add multiple attribute
+        function setAttributes(element, value) {
+            for (const key in value) {
+                element.setAttribute(key, value[key]);
+            }
+        }
+        setAttributes(img, {"id" : "photo", "src" : picture});
+
+
+        const parentEl = document.querySelector('img').parentElement;
+        const imgEL = parentEl.innerHTML;
+        parentEl.innerHTML = '<a href="https://www.google.fr">' + imgEL + '</a>';
+
+        
+
+
+        const h2 = document.createElement( 'h2' );
+        h2.textContent = name;
+
+        const location = document.createElement( 'h3' );
+        location.textContent = city + ', ' + country;
+
+        const tag = document.createElement( 'p' );
+        tag.textContent = tagline;
+
+        const prices = document.createElement( 'span' );
+        prices.textContent = price+'€'+' /jour';
+
+        // article.appendChild(wrapper);
+        article.appendChild(img);
+        article.appendChild(h2);
+        article.appendChild(location);
+        article.appendChild(tag);
+        article.appendChild(prices);
+
         return (article);
     }
     
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,89 +1,34 @@
-    /*async function getPhotographers() {
-        // Penser à remplacer par les données récupérées dans le json
-        const photographers = [
-            {
-                "name": "Mimi Keel",
-                "id": 243,
-                "city": "London",
-                "country": "UK",
-                "tagline": "Voir le beau dans le quotidien",
-                "price": 400,
-                "portrait": "MimiKeel.jpg"
-            },
-            {
-                "name": "Ellie-Rose Wilkens",
-                "id": 930,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Capturer des compositions complexes",
-                "price": 250,
-                "portrait": "EllieRoseWilkens.jpg"
-            },
-            {
-                "name": "Tracy Galindo",
-                "id": 82,
-                "city": "Montreal",
-                "country": "Canada",
-                "tagline": "Photographe freelance",
-                "price": 500,
-                "portrait": "TracyGalindo.jpg"
-            },
-            {
-                "name": "Nabeel Bradford",
-                "id": 527,
-                "city": "Mexico City",
-                "country": "Mexico",
-                "tagline": "Toujours aller de l'avant",
-                "price": 350,
-                "portrait": "NabeelBradford.jpg"
-            },
-            {
-                "name": "Rhode Dubois",
-                "id": 925,
-                "city": "Barcelona",
-                "country": "Spain",
-                "tagline": "Je crée des souvenirs",
-                "price": 275,
-                "portrait": "RhodeDubois.jpg"
-            },
-            {
-                "name": "Marcel Nikolic",
-                "id": 195,
-                "city": "Berlin",
-                "country": "Germany",
-                "tagline": "Toujours à la recherche de LA photo",
-                "price": 300,
-                "portrait": "MarcelNikolic.jpg"
-            }
-        ]
-        // et bien retourner le tableau photographers seulement une fois
-        return ({
-            photographers: [...photographers]})
-    }*/
+function displayData(photographers) {
+    const photographersSection = document.querySelector(".photographer_section");
 
-    async function displayData(photographers) {
-        const photographersSection = document.querySelector(".photographer_section");
+    photographers.forEach((photographer) => {
+        console.log(photographer);
+        const photographerModel = photographerFactory(photographer);
+        const userCardDOM = photographerModel.getUserCardDOM();
+        photographersSection.appendChild(userCardDOM);
+    });
+};
 
-        photographers.forEach((photographer) => {
-            const photographerModel = photographerFactory(photographer);
-            const userCardDOM = photographerModel.getUserCardDOM();
-            photographersSection.appendChild(userCardDOM);
-        });
-    };
+function init() {
+    fetch ("data/photographers.json")
 
-    async function init() {
-        const photographersSection = document.querySelector(".photographer_section");
+    .then((response) => response.json())
 
-        fetch ("data/photographers.json")
-            .then(response => response.json())
-            // .then(data => console.log(data))
-            .then(data => photographersSection = data[photographers])
-
-        // Récupère les datas des photographes
-       /*const { photographers } = await getPhotographers();
-        displayData(photographers);*/
-        displayData(photographers);
-    };
+    .then(
+        (data) => {
+            const { photographers } = data;
+            console.log(photographers);
+            displayData(photographers);
+        }
+    )
     
-    init();
+    .catch(
+        (error) => {
+            console.log(`Error fetching data : ${error}`)
+            document.querySelector('.photographer_section').innerHTML = `Impossible d'afficher les photographes`
+        }
+    )
+};
+
+init();
     
