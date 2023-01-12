03a8152ef21eb760dd47cad23f279e692bc44f2d
KevinSWP <thranva@gmail.com>
2023-01-12 15:17:55 +0100

    correction contact form

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,8 +1,10 @@
+//Function display contact form
 function displayModal() {
     const modal = document.getElementById("contact_modal");
 	modal.style.display = "block";
 }
 
+//Function display off contact form
 function closeModal() {
     const modal = document.getElementById("contact_modal");
     modal.style.display = "none";
