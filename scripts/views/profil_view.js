efe129279aba39b3c82571d8c3423647dad897fa
kevin swp <thranva@gmail.com>
2023-03-09 16:37:22 +0100

    lightbox replace by svg icon

--- scripts/views/profil_view.js
+++ scripts/views/profil_view.js
@@ -178,25 +178,45 @@ export default class PhotographerCardProfil {
         //Fill the DOM
         if (this._photographer.video === undefined) {
             div.innerHTML = `
-                <button tabindex="2" role="link" class="previousSlide" onclick="goToPreviousSlide()">&#60;</button>
+                <!--<button tabindex="2" role="link" class="previousSlide" onclick="goToPreviousSlide()">&#60;</button>-->
+                <button type="button" tabindex="2" role="link" class="previousSlide" onclick="goToPreviousSlide()">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+                        <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"/>
+                    </svg>
+                </button>
                 <figure class="figure">
                     <img role="image" src="${this.photographer.image}">
                     <figcaption role="text" class="figcaption">${this.photographer.title}</figcaption>
                 </figure>
                 <button tabindex="1" role="button" id="btnCloseLightboxModal" class="closeModal"><img src="assets/icons/close.svg" onclick="closeLightbox()"></button>
-                <button tabindex="3" role="link" class="nextSlide" onclick="goToNextSlide()">&#62;</button>
+                <!--<button tabindex="3" role="link" class="nextSlide" onclick="goToNextSlide()">&#62;</button>-->
+                <button tabindex="3" role="link" class="nextSlide" onclick="goToNextSlide()">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+                        <path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"/>
+                    </svg>
+                </button>
             `;
         }
 
         else {
             div.innerHTML = `
-                <button tabindex="2" role="button" class="previousSlide" onclick="goToPreviousSlide()">&#60;</button>
+                <!--<button tabindex="2" role="button" class="previousSlide" onclick="goToPreviousSlide()">&#60;</button>-->
+                <button type="button" tabindex="2" role="link" class="previousSlide" onclick="goToPreviousSlide()">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+                        <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"/>
+                    </svg>
+                </button>
                 <figure class="figure">
                     <video role="video" controls><source src="${this._photographer.video}"></video>
                     <figcaption role="text" class="figcaption">${this.photographer.title}</figcaption>
                 </figure>
                 <button tabindex="1" role="button" class="closeModal"><img src="assets/icons/close.svg" onclick="closeLightbox()"></button>
-                <button tabindex="3" role="link" class="nextSlide" onclick="goToNextSlide()">&#62;</button>
+                <!--<button tabindex="3" role="link" class="nextSlide" onclick="goToNextSlide()">&#62;</button>-->
+                <button tabindex="3" role="link" class="nextSlide" onclick="goToNextSlide()">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+                        <path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"/>
+                    </svg>
+                </button>
             `;
         }
 
